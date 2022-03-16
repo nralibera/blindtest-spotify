d4aeabdfd62c306daa17bcf8984d729bf167201f
Nantomiaro RALIBERA <94388805+nralibera@users.noreply.github.com>
2022-03-16 19:39:51 +0100

    Updates

---
+++ precache-manifest.1a96c582942fc0d0c837e698bf21d4c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b522a5c258fd064f5372de783901f655",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "6969250c67380f9bc77f",
+    "url": "/blindtest-spotify/static/css/main.12217c1b.chunk.css"
+  },
+  {
+    "revision": "405d345c59fad63465c0",
+    "url": "/blindtest-spotify/static/js/2.a80bbd7f.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.a80bbd7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6969250c67380f9bc77f",
+    "url": "/blindtest-spotify/static/js/main.811917c3.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/blindtest-spotify/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/blindtest-spotify/static/media/logo.6a581294.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blindtest-spotify/precache-manifest.af1d10f296fb8bac425bec0e56195ae1.js"
+  "/blindtest-spotify/precache-manifest.1a96c582942fc0d0c837e698bf21d4c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blindtest-spotify/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
